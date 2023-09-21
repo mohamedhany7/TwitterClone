c2b0e551e197a213cd8063d15637fdd7920007be
mohamedhany7 <mohameddhanyy7@gmail.com>
2023-09-21 14:41:09 +0300

    updated navigationBar & added profileView

--- TwitterClone/Controllers/Core/MainTabBarController.swift
+++ TwitterClone/Controllers/Core/MainTabBarController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+    @IBOutlet weak var profileBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavBarImage()
+        profileBarButton.image = UIImage(systemName: "person")
     }
     
+    private func configureNavBarImage(){
+        let size:CGFloat = 36
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.image = UIImage(named:"twitterLogo")
+
+        let middleView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        middleView.addSubview(logoImageView)
+        navigationItem.titleView = middleView
+    }
+    
+    @IBAction func didSelectProfileButton(_ sender: UIBarButtonItem) {
+        let vc = ProfileViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
---
+++ TwitterClone/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  TwitterClone
+//
+//  Created by Mohamed Hany on 21/09/2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Profile"
+    }
+}
