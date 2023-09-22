c0625ed48a21546a116142c86ccc502e2ca2f6f4
mohamedhany7 <mohameddhanyy7@gmail.com>
2023-09-22 19:34:21 +0300

    building Profile view

---
+++ TwitterClone/Views/ProfileTableViewHeader.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeader.swift
+//  TwitterClone
+//
+//  Created by Mohamed Hany on 22/09/2023.
+//
+
+import UIKit
+
+class ProfileTableViewHeader: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
